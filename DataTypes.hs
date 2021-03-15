df2037d8a9d819a8c07113cd79d58e4d15f27199
Tiago Vidal <tiagod3p@gmail.com>
2021-03-14 23:49:15 -0300

    continue studying datatypes and pattern matching

--- DataTypes.hs
+++ DataTypes.hs
@@ -13,12 +13,18 @@ register (Single name) = ("The single person name is: " ++ name ++ ".")
 register (Married name duration) = ("The married person name is: " ++ 
                                   name ++ ", and the duration of the marriage is: " ++ 
                                   show duration ++ " years.")
-
-data Role = Intern | Developer | Engineer | Manager
-data Person = Person { role :: Role, name:: String}
+ 
+data Role = Intern | Developer | Engineer | Manager deriving Show
+data Person = Person { role :: Role, name:: String} deriving Show
 
 salary :: Person -> Float
 salary (Person Intern _) = 1000
 salary (Person Developer _) = 3600
 salary (Person Engineer _) = 6000.25
 salary (Person Manager _) = 10000.50
+
+payslip :: Person -> String
+payslip p = "Name is: " ++ show (name p) ++ 
+            ", role is: " ++ show (role p) ++ 
+            " and salary is: " ++ show (salary p) ++ 
+            "."
