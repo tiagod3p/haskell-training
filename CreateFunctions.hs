64e0db046de12815633d27d71b9cc8ce90f88253
Tiago Vidal <tiagod3p@gmail.com>
2021-04-14 14:03:57 -0300

    studying if statement

--- CreateFunctions.hs
+++ CreateFunctions.hs
@@ -6,9 +6,11 @@ incTwo x = x + 2
 sumThreeNumbers :: Int -> Int -> Int -> Int
 sumThreeNumbers x y z = x + y + z
 
+-- list comprehension
 incTwoArray :: [Int] -> [Int]
 incTwoArray xs = [x + 2 | x <- xs]
 
+-- list comprehension with conditional
 onlyOdd :: [Int] -> [Int]
 onlyOdd xs  = [x | x <- xs, (mod x 2) /= 0]
 
@@ -23,3 +25,15 @@ isOdd x = mod (read x :: Int) 2 /= 0
 
 allLengths :: [String] -> [Int]
 allLengths xs = [length x | x <- xs]
+
+-- if statement (always have an else)
+-- if statements in haskell are expressions (returns a value)
+doubleSmallNumber :: Int -> Int
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+
+-- ' in a function denote a strict version of a function 
+-- or a slightly modified version of a function
+doubleSmallNumber' :: Int -> Int
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
